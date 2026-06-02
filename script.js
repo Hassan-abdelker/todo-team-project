cc321433ffe920198a927d94a7da869dc6d5118f
benmaharip-cloud <benmaharip@gmail.com>
2026-06-02 21:35:12 +0100

    Ajout du script JavaScript de base

---
+++ script.js
@@ -0,0 +1,7 @@
+// Script de base pour la page Étudiante
+document.addEventListener("DOMContentLoaded", function() {
+  const titre = document.querySelector("h1");
+  titre.addEventListener("click", function() {
+    alert("Bienvenue sur la branche JavaScript !");
+  });
+});
